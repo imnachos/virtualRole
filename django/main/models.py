41701b2a7c768f8903eafc71605ff9e6fdb154f5
Ignacio Fernandez Giacometti <fernandez.giacometti@gmail.com>
2018-06-25 23:56:33 -0300

    Test classes

--- django/main/models.py
+++ django/main/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class HeroClass(models.Model):
+    name = models.TextField(max_length=20, blank=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Actor(models.Model):
+    """
+    A simple model class for an actor
+    """
+    # Fields
+    name = models.TextField(max_length=20, blank=False)
+    surname = models.TextField(max_length=20, blank=False)
+    age = models.IntegerField()
+    height = models.IntegerField()
+
+    heroClass = models.ForeignKey(HeroClass, on_delete=models.SET_NULL, blank=True, null=True)
+
+    # Metadata
+    class Meta:
+        ordering = ["-name"]
+
+    def __str__(self):
+        return self.name + ' ' + self.surname
+
+
