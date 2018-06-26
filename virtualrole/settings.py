6889f9671f8bcf87f4c390b24730262311aabe9c
Ignacio Fernandez Giacometti <fernandez.giacometti@gmail.com>
2018-06-26 19:37:28 -0300

    Added host

--- virtualrole/settings.py
+++ virtualrole/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
+ALLOWED_HOSTS = ['virtualrole.herokuapp.com','0.0.0.0', 'localhost']
 
 
 # Application definition
