1e2e2d4b41febee55fedb1da9f407601ffae8e0f
Ignacio Fernandez Giacometti <fernandez.giacometti@gmail.com>
2018-06-26 14:19:56 -0300

    Implemented simple Race and Clases models

--- django/main/admin.py
+++ django/main/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import HeroClass, Actor
+from .models import HeroClass, Actor, Race
 
 admin.site.register(HeroClass)
+admin.site.register(Race)
 admin.site.register(Actor)
--- django/main/views.py
+++ django/main/views.py
@@ -1,5 +1,48 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import HeroClass, Actor, Race
+from django.views import generic
 
 def index(request):
-    return HttpResponse('index response')
+
+    countActors = Actor.objects.all().count()
+    countHeroClasses = Actor.objects.all().count()
+    countFighters = Actor.objects.filter(heroClass__name='Fighter').count()
+
+    return render(
+        request,
+        'index.html',
+        context={'countActors': countActors, 'countHeroClasses': countHeroClasses, 'countFighters': countFighters},
+    )
+
+
+# Actor
+
+class ActorListView(generic.ListView):
+    model = Actor
+
+
+class ActorDetailView(generic.DetailView):
+    model = Actor
+
+
+# Class
+
+class HeroClassListView(generic.ListView):
+    model = HeroClass
+
+
+class HeroClassDetailView(generic.DetailView):
+    model = HeroClass
+
+
+# Race
+
+class RaceListView(generic.ListView):
+    model = Race
+
+
+class RaceDetailView(generic.DetailView):
+    model = Race
+
+
