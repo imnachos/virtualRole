e46c6cbad7a812d6317d26976bc0b3ccdcfe8c44
Ignacio Fernandez Giacometti <fernandez.giacometti@gmail.com>
2018-07-01 02:21:12 -0300

    Added Groups and GroupType
    Added Group
    Added GroupType
    Refactored files to avoid casing errors

--- main/admin.py
+++ main/admin.py
@@ -1,17 +1,24 @@
 from django.contrib import admin
-from .models import HeroClass, Actor, Race, Event, Location, LocationType, Item, ItemProperty, ClassProgress
+from .models import HeroClass, Actor, Race, Event, Location, LocationType, Item, ItemProperty, ClassProgress, Group, GroupType
 
 admin.site.register(HeroClass)
+
 admin.site.register(ClassProgress)
 
 admin.site.register(Race)
+
 admin.site.register(Actor)
+
 admin.site.register(Event)
 
 admin.site.register(Location)
 admin.site.register(LocationType)
 
-admin.site.register(ItemProperty)
 admin.site.register(Item)
+admin.site.register(ItemProperty)
+
+admin.site.register(Group)
+admin.site.register(GroupType)
+
 
 
--- main/urls.py
+++ main/urls.py
@@ -4,27 +4,33 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='home'),
-    path('actors/', views.ActorListView.as_view(), name='actors'),
-    path('actors/<int:pk>', views.ActorDetailView.as_view(), name='actor-detail'),
+    path('actor/', views.ActorListView.as_view(), name='actors'),
+    path('actor/<int:pk>', views.ActorDetailView.as_view(), name='actor-detail'),
 
-    path('races/', views.RaceListView.as_view(), name='races'),
-    path('races/<int:pk>', views.RaceDetailView.as_view(), name='race-detail'),
+    path('race/', views.RaceListView.as_view(), name='races'),
+    path('race/<int:pk>', views.RaceDetailView.as_view(), name='race-detail'),
 
-    path('heroClasses/', views.HeroClassListView.as_view(), name='heroClasses'),
-    path('heroClasses/<int:pk>', views.HeroClassDetailView.as_view(), name='heroClass-detail'),
+    path('heroClass/', views.HeroClassListView.as_view(), name='heroClasses'),
+    path('heroClass/<int:pk>', views.HeroClassDetailView.as_view(), name='heroClass-detail'),
 
-    path('locations/', views.LocationListView.as_view(), name='locations'),
-    path('locations/<int:pk>', views.LocationDetailView.as_view(), name='location-detail'),
+    path('location/', views.LocationListView.as_view(), name='locations'),
+    path('location/<int:pk>', views.LocationDetailView.as_view(), name='location-detail'),
 
-    path('locationTypes/', views.LocationTypeListView.as_view(), name='locationTypes'),
+    path('locationType/', views.LocationTypeListView.as_view(), name='locationTypes'),
     path('locationType/<int:pk>', views.LocationDetailView.as_view(), name='locationType-detail'),
 
-    path('events/', views.EventListView.as_view(), name='events'),
-    path('events/<int:pk>', views.EventDetailView.as_view(), name='event-detail'),
+    path('event/', views.EventListView.as_view(), name='events'),
+    path('event/<int:pk>', views.EventDetailView.as_view(), name='event-detail'),
 
-    path('items/', views.ItemListView.as_view(), name='items'),
-    path('items/<int:pk>', views.ItemDetailView.as_view(), name='item-detail'),
+    path('item/', views.ItemListView.as_view(), name='items'),
+    path('item/<int:pk>', views.ItemDetailView.as_view(), name='item-detail'),
 
     path('itemProperty/', views.ItemPropertyListView.as_view(), name='itemProperties'),
-    path('itemProperties/<int:pk>', views.ItemPropertyDetailView.as_view(), name='itemProperty-detail'),
+    path('itemProperty/<int:pk>', views.ItemPropertyDetailView.as_view(), name='itemProperty-detail'),
+
+    path('group/', views.GroupListView.as_view(), name='groups'),
+    path('group/<int:pk>', views.GroupDetailView.as_view(), name='group-detail'),
+
+    path('groupType/', views.GroupTypeListView.as_view(), name='groupTypes'),
+    path('groupType/<int:pk>', views.GroupTypeDetailView.as_view(), name='groupType-detail'),
 ]
